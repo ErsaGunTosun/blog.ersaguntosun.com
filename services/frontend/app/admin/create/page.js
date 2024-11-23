b75af89eef842526db5a1e394dcdb56e4ac878dd
Fatih Tosun <fatihtosun@Moon.local>
2024-11-23 19:08:34 +0300

    UPDATE post create page function post create page added items and function

--- services/frontend/app/admin/create/page.js
+++ services/frontend/app/admin/create/page.js
@@ -1,50 +1,92 @@
-"use client"
-import React from 'react'
+'use client';
+import React, { use } from 'react'
+import { useRouter } from 'next/navigation';
 import MarkdownEditor from '@uiw/react-markdown-editor';
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
 
-const mdStr = `# This is a H1  \n## This is a H2  \n###### This is a H6`;
+import CategoriesDropdown from '@/components/CategoriesDropdown';
 
 function createPage() {
-  const [isFieldVisible, setIsFieldVisible] = React.useState(false)
+  const [isFieldVisible, setIsFieldVisible] = React.useState(true)
+  const editorRef = React.useRef(null)
+  const [mdStr, setMdStr] = React.useState(`# This is a H1  \n## This is a H2  \n###### This is a H6`)
+  const [title, setTitle] = React.useState('')
+  const [category, setCategory] = React.useState([])
+
+  const router = useRouter()
+
+  const handleEditorChange = (value, viewUpdate) => {
+    setMdStr(value)
+  }
+
+  const handlePublish = () => {
+    console.log('Publishing post')
+    console.log('Title:', title)
+    console.log('Category:', category)
+    console.log('Content:', mdStr)
+  }
   return (
     <div className='w-full h-full'>
       <div className='w-full h-full flex flex-col px-10'>
-        <div className='font-medium py-5 text-3xl  text-center mb-4'>Create Post</div>
+        <div className='font-medium py-2 text-3xl text-center mb-3 '>Create Post</div>
 
         <div className='w-full' >
+
           <button onClick={() => setIsFieldVisible(!isFieldVisible)}
             type="button" className="flex text-xl items-center justify-between w-full py-2.5 font-medium text-black custom-border gap-3 mb-4">
             <span>Details</span>
-            <svg className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
-            </svg>
+            {
+              isFieldVisible ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowUp />
+            }
           </button>
 
           <div id="accordion-flush-body-1" className={isFieldVisible ? "block" : "hidden"} aria-labelledby="accordion-flush-heading-1">
-            <div className="py-5 border-b border-gray-200 dark:border-gray-700">
-              <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-              <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+            <div className=" px-2 border-b border-gray-200 dark:border-gray-700">
+
+              <div className="relative z-0 w-full mb-5 group text-black">
+                <input autoComplete='off' type="text" name="floating_title" id="floating_title" value={title} onChange={(e) => setTitle(e.target.value)}
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-200 appearance-non peer outline-none" placeholder=" " required />
+                <label htmlFor="floating_title" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Post Title</label>
+              </div>
+
+
+              <div className="relative z-0 w-full mb-5 group text-black">
+                <CategoriesDropdown category={category} setCategory={setCategory} />
+              </div>
+
             </div>
           </div>
+
         </div>
 
-        <div className='w-full h-full'>
+        <div className='w-full h-full' >
           <div className='font-medium text-xl custom-border py-2.5 mb-4'>Content</div>
           <MarkdownEditor
-            className='w-full h-full bg-white'
+            ref={editorRef}
+            className='w-full h-full max-h-full bg-white'
             visible={true}
             enablePreview={true}
             value={mdStr}
-            onChange={(value, viewUpdate) => {
-              console.log(value, viewUpdate)
-            }}
+            enableScroll={true}
+            onChange={(value, viewUpdate) => handleEditorChange(value, viewUpdate)}
           />
+
+          <div className='w-full flex justify-end'>
+            <button onClick={() => { router.back() }}
+              type="button" className="text-red-700 hover:underline hover:underline-offset-2 hover:decoration-1 hover:decoration-dotted font-bold rounded-lg text-sm px-3 py-2.5 text-center mb-2">
+              Exit Without Saving
+            </button>
+            <button onClick={() => handlePublish()}
+              type="button" className="text-green-700 hover:underline hover:underline-offset-2 hover:decoration-1 hover:decoration-dotted font-bold rounded-lg text-sm px-3 py-2.5 text-center mb-2">
+              Publish Post
+            </button>
+          </div>
         </div>
 
 
       </div>
 
-    </div>
+    </div >
   )
 }
 
---
+++ services/frontend/components/CategoriesDropdown.js
@@ -0,0 +1,130 @@
+'use client';
+import { useState, useRef } from "react";
+
+const CategoriesDropdown = ({category, setCategory}) => {
+    const [inputValue, setInputValue] = useState("");
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const [filteredOptions, setFilteredOptions] = useState([]);
+    const [options, setOptions] = useState(["One", "Two", "Three", "Four", "Five"]);
+    const dropdownRef = useRef(null);
+
+
+
+    const handleInputChange = (e) => {
+        const query = e.target.value.toLowerCase();
+        setInputValue(e.target.value);
+
+        if (query) {
+            const filtered = options.filter(
+                (option) =>
+                    option.toLowerCase().includes(query) &&
+                    !category.includes(option)
+            );
+            setFilteredOptions(filtered);
+            setIsDropdownVisible(true);
+           
+        } else {
+            setFilteredOptions([]);
+            setIsDropdownVisible(false);
+        }
+
+    };
+
+    // Bir seçenek seçildiğinde
+    const handleOptionClick = (option) => {
+        if (!category.includes(option)) {
+            setCategory((prev) => [...prev, option]);
+            setInputValue("");
+            setFilteredOptions([]);
+            setIsDropdownVisible(false);
+        }
+    };
+
+    const handleBadgeRemove = (option) => {
+        setCategory((prev) => prev.filter((item) => item !== option));
+    };
+
+    const handleAddNewOption = () => {
+        if (inputValue && !options.includes(inputValue)) {
+          setOptions((prev) => [...prev, inputValue]); 
+          setCategory((prev) => [...prev, inputValue]); 
+          setInputValue("");
+          setIsDropdownVisible(false); 
+        }
+      };
+
+    const handleClickOutside = (e) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+            setIsDropdownVisible(false);
+        }
+    };
+
+    useState(() => {
+        window.document.addEventListener("mousedown", handleClickOutside);
+        return () => window.document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+    return (
+        <div className="relative w-full custom-border" ref={dropdownRef}>
+            <div className="flex items-center flex-wrap gap-2 border border-gray-500 rounded-md py-2.5 bg-transparent">
+
+                {category.map((option, index) => (
+                    <span
+                        key={index}
+                        className="flex items-center px-3 py-1 text-sm text-gray-800 bg-gray-100 rounded"
+                    >
+                        {option}
+                        <button
+                            onClick={() => handleBadgeRemove(option)}
+                            className="ml-2 text-black hover:text-gray-300"
+                        >
+                            &times;
+                        </button>
+                    </span>
+                ))}
+
+                <div className="relative flex-1">
+                    <input
+                        autoComplete="off"
+                        type="text"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onFocus={() => {if(inputValue.length > 0) setIsDropdownVisible(true)}}
+                        id="floating_category"
+                        name="floating_category"
+                        placeholder=""
+                        className="block appearance-non w-full peer bg-transparent text-sm text-black focus:outline-none placeholder:text-gray-500"
+                    />
+                    <label htmlFor="floating_category" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 bottom-0 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Category to search...</label>
+                </div>
+            </div>
+
+            {/* Dropdown */}
+            {isDropdownVisible && (
+                <ul
+                    className="absolute z-10 mt-1 w-full bg-white text-black border border-gray-400  shadow-lg max-h-60 overflow-auto"
+                >
+                    {filteredOptions.length >0 ? filteredOptions.map((option, index) => (
+                        <li
+                            key={index}
+                            onClick={() => handleOptionClick(option)}
+                            className="cursor-pointer hover:bg-gray-200 px-3 py-2"
+                        >
+                            {option}
+                        </li>)) 
+                        : 
+                        (<li
+                            onClick={handleAddNewOption}
+                            className="cursor-pointer hover:bg-gray-200 px-3 py-2"
+                        >
+                            Add "{inputValue}" to the list
+                        </li>
+                    )}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default CategoriesDropdown;
